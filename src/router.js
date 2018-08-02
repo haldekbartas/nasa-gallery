2b0db0cc2d027cd5856f34a98ef9980ad0c1be39
haldasbartlomiej <haldas.bartlomiej@gmail.com>
2018-08-02 12:51:49 +0200

    create basic app view

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 
+
 Vue.use(Router);
 
 export default new Router({
@@ -10,12 +11,12 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: Home,
     },
     {
       path: '/about',
-      name: 'about',
+      name: 'About',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
