fc91191810e95f538001420ffda46d4b922d132d
haldasbartlomiej <haldas.bartlomiej@gmail.com>
2018-08-02 20:30:57 +0200

    finished app

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,172 @@
+Skip to content Search or jump to… Pull requests Issues Marketplace Explore @haldekbartas Sign out 0 1 1 borbah/spacer-helloroman Code Issues 0 Pull requests 0 Projects 0 Wiki Insights spacer-helloroman/src/App.vue 7583957 18 days ago @borbah borbah Finished app 170 lines (153 sloc) 3.49 KB
 <template>
-  <div class="app">
-  
-   <div class="nav">
-       <router-link to="/">home</router-link>
-          <router-link to="/about">about</router-link>
-   </div>
-   
-    <router-view></router-view>
+  <div :class="[{ flexStart: step === 1 }, 'wrapper']">
+    <transition name="slide">
+      <img src="./assets/logo.svg" class="logo" v-if="step === 1">
+    </transition>
+    <transition name="fade">
+      <HeroImage v-if="step === 0" />
+    </transition>
+    <Claim v-if="step === 0" />
+    <SearchInput v-model="searchValue" @input="handleInput" :dark="step === 1" />
+    <div class="results" v-if="results && !loading && step === 1">
+      <Item v-for="item in results" :item="item" :key="item.data[0].nasa_id" @click.native="handleModalOpen(item)" />
+    </div>
+    <div class="loader" v-if="step === 1 && loading" />
+    <Modal v-if="modalOpen" :item="modalItem" @closeModal="modalOpen = false" />
   </div>
 </template>
+<script>
+    import axios from 'axios';
+    import debounce from 'lodash.debounce';
+    import Claim from '@/components/Claim.vue';
+    import SearchInput from '@/components/SearchInput.vue';
+    import HeroImage from '@/components/HeroImage.vue';
+    import Item from '@/components/Item.vue';
+    import Modal from '@/components/Modal.vue';
+    const API = 'https://images-api.nasa.gov/search';
+    export default {
+        name: 'App',
+        components: {
+            Modal,
+            HeroImage,
+            Item,
+            Claim,
+            SearchInput,
+        },
+        data() {
+            return {
+                modalOpen: false,
+                modalItem: null,
+                loading: false,
+                step: 0,
+                searchValue: '',
+                results: [],
+            };
+        },
+        methods: {
+            handleModalOpen(item) {
+                this.modalOpen = true;
+                this.modalItem = item;
+            },
+            // eslint-disable-next-line
+            handleInput: debounce(function() {
+                this.loading = true;
+                console.log(this.searchValue);
+                axios.get(`${API}?q=${this.searchValue}&media_type=image`)
+                    .then((response) => {
+                        this.results = response.data.collection.items;
+                        this.loading = false;
+                        this.step = 1;
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }, 500),
+        },
+    };
 
+</script>
 <style lang="scss">
     @import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,600,800');
-    
-    *{
+    * {
         box-sizing: border-box;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
     }
-    body{
+
+    body {
         font-family: 'Montserrat', sans-serif;
-        margin:0;
-        padding:0;
+        margin: 0;
+        padding: 0;
+    }
+
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity .3s ease;
+    }
+
+    .fade-enter,
+    .fade-leave-to {
+        opacity: 0;
+    }
+
+    .slide-enter-active,
+    .slide-leave-active {
+        transition: margin-top .3s ease;
+    }
+
+    .slide-enter,
+    .slide-leave-to {
+        margin-top: -50px;
+    }
+
+    .wrapper {
+        margin: 0;
+        position: relative;
+        width: 100%;
+        min-height: 100vh;
+        padding: 30px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        &.flexStart {
+            justify-content: flex-start;
+        }
+    }
+
+    .loader {
+        margin-top: 100px;
+        display: inline-block;
+        width: 64px;
+        height: 64px;
+        @media (min-width: 768px) {
+            width: 90px;
+            height: 90px;
+        }
+    }
+
+    .loader:after {
+        content: " ";
+        display: block;
+        width: 46px;
+        height: 46px;
+        margin: 1px;
+        border-radius: 50%;
+        border: 5px solid #1e3d4a;
+        border-color: #1e3d4a transparent #1e3d4a transparent;
+        animation: loading 1.2s linear infinite;
+        @media (min-width: 768px) {
+            width: 90px;
+            height: 90px;
+        }
+    }
+
+    @keyframes loading {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+
+    .logo {
+        position: absolute;
+        top: 30px;
+    }
+
+    .results {
+        margin-top: 50px;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 20px;
+        @media (min-width: 768px) {
+            width: 90%;
+            grid-template-columns: 1fr 1fr 1fr;
+        }
     }
 
 </style>
+© 2018 GitHub, Inc. Terms Privacy Security Status Help Contact GitHub API Training Shop Blog About Press h to open a hovercard with more details.
--- src/components/Claim.vue
+++ src/components/Claim.vue
@@ -28,18 +28,33 @@
 
 
 <style lang="scss" scoped>
-    .claimWrapper{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
+  .claimWrapper {
+    display: flex;
+    text-align: center;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: white;
+  }
+  .claim {
+    font-size: 50px;
+    margin: 0;
+    @media (min-width: 768px) {
+      font-size: 80px;
     }
-    .claim{
-        font-size: 50px;
-            }
-    .subclaim{
-        margin: 25px 0 0 0;
-        font-size: 13px;
-        
+    @media (min-width: 1024px) {
+      font-size: 100px;
     }
+  }
+  .subclaim {
+    margin: 20px 0 0 0;
+    font-size: 13px;
+    font-weight: 400;
+    @media (min-width: 768px) {
+      font-size: 17px;
+      width: 500px;
+      line-height: 1;
+      letter-spacing: 1px;
+    }
+  }
 </style>
